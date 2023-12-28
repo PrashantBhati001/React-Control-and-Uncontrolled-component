b534da9725c04f2f0a8701fddcc731173e35d6e4
Prashant Bhati <prashantbsmvit070@gmail.com>
2023-12-28 14:10:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState,useRef} from 'react';
+
+/*
+
+// Controlled component
 
 function App() {
+const [firstname,setFirstname]=useState("Prashant")
+const [lastname,setLastname]=useState("Bhati")
+
+function handlefirstname(e)
+{
+  console.log(e.target.value);
+  setFirstname(e.target.value)
+}
+
+function handlelastname(e)
+{
+  console.log(e.target.value);
+  setLastname(e.target.value)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input type="text" value={firstname} onChange={handlefirstname} />
+        <br />
+        <input type="text" value={lastname} onChange={handlelastname} />
+      </form>
+      
     </div>
   );
 }
 
+
+
+*/
+
+
+//Uncontrolled component
+
+function App() {
+  
+  const firstref=useRef(null)
+  const secondref=useRef(null)
+
+  function handlesubmit(e)
+  {
+   
+    e.preventDefault()
+    console.log(firstref.current.value);
+    console.log(secondref.current.value);
+    const input=document.getElementById("rollno").value
+    console.log(input);
+  }
+  
+    return (
+      <div className="App">
+        <form onSubmit={handlesubmit}>
+          <input type="text" ref={firstref} />
+          <br />
+          <input type="text"  ref={secondref} />
+          <br />
+          <input type="text"  id="rollno" />
+          <br />
+          <button>Submit</button>
+        </form>
+        
+      </div>
+    );
+  }
 export default App;
